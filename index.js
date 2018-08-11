c3c41a6d8ee85104119837e935996b14d6f4d62c
HolSeo <dseo1004@gmail.com>
2018-08-10 20:13:54 -0400

    hello world

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req,res) => {
+    res.send('HELLO WORLD!')
+})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT)
