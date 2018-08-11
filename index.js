70da27688fc70102997eae8bae86309230d8599c
HolSeo <dseo1004@gmail.com>
2018-08-10 23:48:03 -0400

    added API keys

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+require('./services/passport')
 
 const app = express();
+
 
 app.get('/', (req,res) => {
     res.send('HELLO WORLD!')
