54672cd8f20b855f6bc78f2348fb72f43aadb90d
HolSeo <dseo1004@gmail.com>
2018-08-12 23:19:00 -0400

    materializecss

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
     render() {
         return (
             <BrowserRouter>
-                <div>
+                <div className="container">
                     <Header />
                     <Route exact path="/" component={Landing} />
                     <Route exact path="/surveys" component={Dashboard} />
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,9 +3,18 @@ import React, { Component } from 'react'
 class Header extends Component {
     render() {
         return (
-            <div>
-                THIS IS THE HEADER!
-            </div>
+            <nav>
+                <div style={{ margin: '0 10px' }}>
+                    <div className="nav-wrapper">
+                        <a href="#" className="left brand-logo">SurveyMe</a>
+                        <ul id="nav-mobile" className="right">
+                            <li><a href="sass.html">Sass</a></li>
+                            <li><a href="badges.html">Components</a></li>
+                            <li><a href="collapsible.html">JavaScript</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         )
     }
 }
