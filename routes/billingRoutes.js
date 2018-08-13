128a656d320583f1cadf97bbd69c3265b31a30cd
HolSeo <dseo1004@gmail.com>
2018-08-13 15:26:18 -0400

    updates credit and finished billing

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,14 +1,17 @@
 const keys = require('../config/keys')
 const stripe = require('stripe')(keys.stripeSecretKey)
+const requiredLogin = require('../middlewares/requireLogin')
 
 module.exports = (app) => {
-    app.post('/api/stripe', async (req,res) => {
+    app.post('/api/stripe', requiredLogin, async (req,res) => {
         const charge = await stripe.charges.create({
             amount: 500,
             currency: 'usd',
             description: '$5 for 5 email credits',
             source: req.body.id,
         });
-        console.log(charge)
+        req.user.credits += 5; // passport in index.js
+        const user = await req.user.save()
+        res.send(user);
     })
 }
