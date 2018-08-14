6a26133e873b3a7c086f0cf4ae40daa2355b92a2
HolSeo <dseo1004@gmail.com>
2018-08-13 22:08:28 -0400

    commit to heroku

--- index.js
+++ index.js
@@ -35,5 +35,16 @@ app.use(passport.session())
 require('./routes/authRoutes')(app)
 require('./routes/billingRoutes')(app)
 
+if (process.env.NODE_ENV === 'production') {
+    // Express first checks the client/build if route isn't recognized.
+    app.use(express.static('client/build'))
+
+    // Express serves index.html if nothign matches. 
+    const path = require('path')
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT)
