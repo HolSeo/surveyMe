226357db2eb0c75f8dce16600f9d7c89949cd9d8
HolSeo <dseo1004@gmail.com>
2018-08-17 20:11:35 -0400

    toggle visibility

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react'
 import SurveyForm from './SurveyForm';
 import SurveyFormReview from './SurveyFormReview';
+import { reduxForm } from 'redux-form'
 
 class SurveyNew extends Component {
+    state = { showFormReview: false }
+
+    renderContent() {
+        if (this.state.showFormReview) {
+            return <SurveyFormReview onCancel={() => this.setState({ showFormReview: false })}/>
+        }
+        return <SurveyForm onSurveySubmit={() => this.setState({ showFormReview: true })}/>
+    }
+
     render() {
         return (
             <div>
-                <SurveyForm />
-                <SurveyFormReview />
+                {this.renderContent()}
             </div>
         )
     }
 }
 
-export default SurveyNew
+// Navigate away from this component, dump all form values.
+export default reduxForm({
+    form: 'surveyForm'
+})(SurveyNew)
