c446a62b3dab93e8fac188d3fa3e91261dc0adc7
HolSeo <dseo1004@gmail.com>
2018-08-18 17:14:33 -0400

    webhook

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -9,19 +9,19 @@ module.exports = (survey) => {
                     <p>Please rate our services</p>
                     <p>${survey.body}</p>
                     <div>
-                        <a href="${keys.redirectDomain}/api/surveys/response">5 Stars</a>
+                        <a href="${keys.redirectDomain}/api/surveys/${survey.id}/fivestar">5 Stars</a>
                     </div>
                     <div>
-                        <a href="${keys.redirectDomain}/api/surveys/response">4 Stars</a>
+                        <a href="${keys.redirectDomain}/api/surveys/${survey.id}/fourstar">4 Stars</a>
                     </div>
                     <div>
-                        <a href="${keys.redirectDomain}/api/surveys/response">3 Stars</a>
+                        <a href="${keys.redirectDomain}/api/surveys/${survey.id}/threestar">3 Stars</a>
                     </div>
                     <div>
-                        <a href="${keys.redirectDomain}/api/surveys/response">2 Stars</a>
+                        <a href="${keys.redirectDomain}/api/surveys/${survey.id}/twostar">2 Stars</a>
                     </div>
                     <div>
-                        <a href="${keys.redirectDomain}/api/surveys/response">1 Star</a>
+                        <a href="${keys.redirectDomain}/api/surveys/${survey.id}/onestar">1 Star</a>
                     </div>
                 </div>
             </body>
